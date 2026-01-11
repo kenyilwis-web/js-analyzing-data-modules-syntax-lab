16f4acaff3eeca1f880c0a9b0a191f28a353f522
kenyilwis-web <kenyilwis@gmail.com>
2026-01-11 23:13:39 +0300

    datamodules

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require('datejs');
 
+function combineUsers(...args) {
+  // Step 2: Initialize the Return Object
+  const combinedObject = {
+    users: []
+  };
 
+  // Step 3 & 4: Loop through args and merge arrays using spread operator
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  // Step 5: Get today's date (formatted M/d/yyyy)
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Step 7: Return object
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
